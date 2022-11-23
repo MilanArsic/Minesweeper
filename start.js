17347b03e00a16a4c9b235b9ac7e75eb122cbca9
Milan Arsic <104004527+MilanArsic@users.noreply.github.com>
2022-11-23 10:13:12 +0100

    Update start.js

--- start.js
+++ start.js
@@ -1,34 +1,48 @@
-
-
-function makeArray(cols, rows) {
+function makeArray(cols, rows){
     var arr = new Array(cols);
-    for (var i=0; i<arr.length; i++) {
-        arr[i] = new Array(rows);
-    }
-    return arr;
+  for (var i=0; i < arr.length; i++){
+    arr[i]= new Array(rows);
+  }
+  return arr;
 }
 
-var Grid;
-var cols = 15;
-var rows = 15;
+var grid;
+var cols;
+var rows;
+var w = 20;
 
-function Setup(){
-    createCanvas (150, 150);
-    Grid = makeArray(cols, rows);
-    for (var i = 0; i < cols; i++){
-       for (var j = 0; j< rows; j++){
-        Grid[i][j] = new Cell();
-       }     
-        }
+function setup() {
+  createCanvas(201, 201);
+  cols = floor(width / w);
+  rows = floor(height / w);
+  grid = makeArray (cols, rows);
+  for (var i=0; i < cols; i++) {
+    for (var j=0; j < rows; j++){
+        grid[i][j] = new Cell(i, j, w);
     }
-
-
-function draw(){
-    background(0);
-    for (var i=0; i<cols; i++){
-        for (var j=0; j<rows; j++){
-            Grid[i][j].show();
-        }
+  }
+  for (var i=0; i < cols; i++) {
+     for (var j=0; j < rows; j++){
+        grid[i][j].countMines();
+}
+}
+}
+function mouse() {
+  for (var i = 0; i < cols; i++) {
+    for (var j = 0; j < rows; j++) {
+      if (grid[i][j].contains(mouseX, mouseY))
+      grid[i][j].reveal();
     }
+  }
+  
 }
 
+ 
+function draw() {
+  background(255);
+  for (var i=0; i < cols; i++){
+    for (var j=0; j < rows; j++){
+        grid [i][j].show();
+    }
+  }
+}
