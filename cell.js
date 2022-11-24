4aa104ebf92ec86ee10e3ed6c8c91ae8107fc854
Milan Arsic <104004527+MilanArsic@users.noreply.github.com>
2022-11-24 15:58:49 +0100

    Update cell.js

--- cell.js
+++ cell.js
@@ -57,7 +57,7 @@ Cell.prototype.countMines = function() {
 };
 
 Cell.prototype.contains = function(x, y){
-  return x > this.x && x < this.x + this.w && y > this.y && y < this.y + this.w;
+  return (x > this.x && x < this.x + this.w && y > this.y && y < this.y + this.w);
 }; 
 Cell.prototype.reveal = function(){
   this.revealed = true;
