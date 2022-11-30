220c646767095fcf2f435a452ff5979d2bc14337
Milan Arsic <104004527+MilanArsic@users.noreply.github.com>
2022-11-30 15:46:21 +0100

    Update start.js

--- start.js
+++ start.js
@@ -1,3 +1,4 @@
+ 
 function makeArray(cols, rows){
     var arr = new Array(cols);
   for (var i=0; i < arr.length; i++){
@@ -11,6 +12,8 @@ var cols;
 var rows;
 var w = 20;
 
+var totalMines = 20 ;
+
 function setup() {
   createCanvas(201, 201);
   cols = floor(width / w);
@@ -21,20 +24,49 @@ function setup() {
         grid[i][j] = new Cell(i, j, w);
     }
   }
+
+   var options = [];
+   for (var i = 0; i < cols; i++) {
+    for (var j = 0; j < rows; j++) {
+      options.push([i, j]);
+    }
+   }
+
+  for (var n = 0; n < totalMines; n++){
+    var index = floor(random(options.length));
+    var choice = options[index];
+    var i = choice[0];
+    var j = choice[1];
+    options.splice(index, 1);
+    grid[i][j].mine = true;
+  }
+   
   for (var i=0; i < cols; i++) {
      for (var j=0; j < rows; j++){
         grid[i][j].countMines();
 }
 }
 }
+
+function gameOver() {
+  for (var i=0; i < cols; i++) {
+    for (var j=0; j < rows; j++){
+       grid[i][j].reveal = true;
+    }
+  }
+}
 function mousePressed() {
   for (var i = 0; i < cols; i++) {
     for (var j = 0; j < rows; j++) {
-      if (grid[i][j].contains(mouseX, mouseY))
+      if (grid[i][j].contains(mouseX, mouseY)){
       grid[i][j].reveal();
+
+      if (grid[i][j].mine){
+        gameOver();
+      }
     }
   }
-  
+}
 }
 
  
@@ -46,3 +78,4 @@ function draw() {
     }
   }
 }
+
