481b5e375af579ae45056b28ffee6cade8b1e4fe
Milan Arsic <104004527+MilanArsic@users.noreply.github.com>
2022-11-30 15:47:24 +0100

    Update cell.js

--- cell.js
+++ cell.js
@@ -1,17 +1,15 @@
 function Cell(i,j,w) {
   this.i = i;
   this.j = j;
-  this.x = i*w;
-  this.y = j*w;
+  this.x = i * w;
+  this.y = j * w;
   this.w = w;
   this.aroundCount = 0;
 
-  if (random (1) < 0.5){
-  this.mine = true;
-  } else {
-    this.mine = false;
-  }
+  this.mine = false;
   this.revealed = false;
+
+  this.flagged = true;
 }
 
 Cell.prototype.show = function() {
@@ -21,15 +19,17 @@ Cell.prototype.show = function() {
    if(this.revealed){
     if(this.mine){
       fill (100);
-      ellipse(this.x + this.w*0.5, this.y+this.w*0.5, this.w*0.5);
+      ellipse(this.x + this.w * 0.5, this.y + this.w * 0.5, this.w * 0.5);
        } else{
         fill (200);
         rect(this.x, this.y, this.w, this.w);
+        if (this.aroundCount > 0) {
         textAlign(CENTER);
         fill(0);
-        text(this.aroundCount, this.x + this.w*0.5, this.y + this.w);
+        text(this.aroundCount, this.x + this.w * 0.5, this.y + this.w-5);
        }
 }
+   }
 };
 
 Cell.prototype.countMines = function() {
@@ -43,7 +43,7 @@ Cell.prototype.countMines = function() {
        if (i < 0 || i >= cols) continue;
 
   for (var yoff = -1; yoff <= 1; yoff++) {
-      var j = this.y + yoff;
+      var j = this.j + yoff;
       if (j < 0 || j >= rows) continue;
 
       var around = grid[i][j];
@@ -61,4 +61,22 @@ Cell.prototype.contains = function(x, y){
 }; 
 Cell.prototype.reveal = function(){
   this.revealed = true;
+  if (this.aroundCount == 0) {
+    this.fill();
+  }
+}
+Cell.prototype.fill = function(){
+  for (var xoff = -1; xoff <= 1; xoff++) {
+    for (var yoff = -1; yoff <= 1; yoff++) {
+    var i = this.i + xoff;
+   var j = this.j + yoff;
+   if (i > -1 && i < cols && j > -1 && j < rows){
+
+      var around = grid[i][j];
+      if (!around.mine && !around.revealed ) {
+        around.reveal();
+      }
+    }
+      }
+   }
 };
